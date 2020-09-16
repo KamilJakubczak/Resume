05de63786aa805ea1ab4fc99f92333859a00758d
kamiljakubczak <kamiljakubczak@outlook.com>
2020-09-16 16:06:30 +0200

    Working on aws s3 deploy

--- resume/settings.py
+++ resume/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#  TATIC_URL = '/static/'
 #  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
