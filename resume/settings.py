56516eaf22c0880a6ce0fa900773f2065fcd1a18
kamiljakubczak <kamiljakubczak@outlook.com>
2020-09-16 20:13:04 +0200

    Setting up s3

--- resume/settings.py
+++ resume/settings.py
@@ -156,5 +156,6 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'kamil-jakubczak-static'
 AWS_S3_REGION_NAME = 'eu-west-2'
+
 AWS_ACCESS_KEY_ID = 'AKIAQT55NUKGUQRUCZ3F'
 AWS_SECRET_ACCESS_KEY = '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'
