8a5a3b227203c0bb8e7245c9b8c1c23c2e868c40
kamiljakubczak <kamiljakubczak@outlook.com>
2020-09-16 20:30:59 +0200

    Changing region s3

--- resume/settings.py
+++ resume/settings.py
@@ -155,7 +155,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'kamil-jakubczak-static'
-AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_REGION_NAME = 'eu-central-1'
 
 AWS_ACCESS_KEY_ID = 'AKIAQT55NUKGUQRUCZ3F'
 AWS_SECRET_ACCESS_KEY = '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'
